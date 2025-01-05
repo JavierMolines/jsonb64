f471d7132ba66ca5483685b468ad8f3096b5c78c
JavierMolines <eljavimolines@gmail.com>
2025-01-05 10:50:57 -0300

    feat(other): remove unused files

--- src/app/constants/main.ts
+++
@@ -1,2 +0,0 @@
-export const ID_CLIPBOARDS_ITEMS = "data";
-export const OPTIONS_BUTTON_CHECK = "optionSaveRedirect";
--- src/app/utils/methods.ts
+++ src/app/utils/methods.ts
@@ -1,13 +1,3 @@
-export const makeId = () => {
-	const validCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	let keyGen = "";
-	for (let i = 0; i < 20; i++) {
-		const indexRandom = Math.floor(Math.random() * validCharacters.length);
-		keyGen += validCharacters.charAt(indexRandom);
-	}
-	return keyGen;
-};
-
 export const copyClipboard = (data: string) => {
 	navigator.clipboard
 		.writeText(data)
